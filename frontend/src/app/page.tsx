b65159a06a605533afd4abbec71fd01e0b9cae45
AHONYO Israel <izy.clapp@gmail.com>
2023-10-23 16:12:59 +0000

    navbar and first section completed

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,12 +1,18 @@
-import Navbar from '../components/navbar/navbar'
+import React from 'react';
+import FirstSection from '@/components/home-section1/firstSection';
+import Navbar from '../components/navbar/navbar';
 
 const Home = () => {
- return(
-  <>
-<Navbar/>
-  
-  </>
- )
+  return (
+    <>
+      <div className=''>
+        <Navbar />
+      </div>
+      <div className="flex justify-center mt-10"> 
+        <FirstSection />
+      </div>
+    </>
+  );
 };
 
 export default Home;
--- frontend/src/components/navbar/navbar.tsx
+++ frontend/src/components/navbar/navbar.tsx
@@ -1,25 +1,53 @@
+import Link from 'next/link';
+import Image from 'next/image';
 
-const Navbar =()=>{
-    return (
-        <nav className= " p-4 my-2 mx-2  " >
-        <div className="container mx-auto flex justify-between items-center w-4/5 my-2  " >
-          <div className ="flex justify-between" >
-          <h1 className=" text-2xl font-bold mr-4" > Xmentor < /h1>
-            < ul className = "flex space-x-4 m-6 " >
-              <li>Mentors < /li>
-              < li > Questions < /li>
-              < li > Ressources < /li>
-              < li > Job < /li>
-              < li > A Propos < /li>
-                < /ul>
-                < /div>
-                < ul className = "flex space-x-4 " >
-                  <li>Login < /li>
-                  < li > Signup < /li>
-                  < /ul>
-                  < /div>
-                  < /nav>
-      );
+const Navbar = () => {
+  return (
+    <nav className="p-4 my-2 mx-2">
+      <div className="container mx-auto flex justify-between items-center w-4/5 my-2">
+        <div className="flex items-center">
+          <Image
+            src="/xmentor.svg"
+            className="mx-auto"  
+            alt="xmentor-logo"
+            width={100}
+            height={50}
+          />
+          <ul className="flex space-x-4 ml-10">
+            <li>
+              <Link href="/">Mentors</Link>
+            </li>
+            <li>
+              <Link href="/">Questions</Link>
+            </li>
+            <li>
+              <Link href="/">Ressources</Link>
+            </li>
+            <li>
+              <Link href="/">Job</Link>
+            </li>
+            <li>
+              <Link href="/">A Propos</Link>
+            </li>
+          </ul>
+        </div>
+        <div className="flex space-x-4 items-center">
+          <ul>
+            <li>
+              <Link href="/">Se Connecter</Link>
+            </li>
+          </ul>
+          <ul>
+            <li>
+              <Link href="/"  >
+                <p className="rounded-full bg-purple-500 text-white p-2">Inscription</p>
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
 }
 
-export default Navbar
+export default Navbar;
